41413f54d04e5cdaccac39f8aee06da826590833
Tue Munch Madsen <tuepvm@gmail.com>
2022-08-30 13:54:11 +0200

    Add project files.

---
+++ Robotterne kommer/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Robotterne_kommer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string model;
+            string color;
+            bool wifi = false;
+            string wifiYesNo;
+            byte batterySize = 0;
+            bool battery = false;
+            bool soap = false;
+            byte wheels = 0;
+
+            Console.WriteLine("Hvilken model robot vil du købe");
+            Console.WriteLine("Roomba, Vinduspudser, Dækskifter");
+            model = Console.ReadLine().ToLower();
+
+
+            Console.WriteLine("\r\nVælg farve");
+            Console.WriteLine("Hvid, Rød, Blå, Grøn");
+            color = Console.ReadLine().ToLower();
+
+
+            Console.WriteLine("\r\nVil du ha wifi i robotten");
+            Console.WriteLine("y for ja og n for nej");
+            wifiYesNo = Console.ReadLine().ToLower();
+
+
+            if (wifiYesNo == "y")
+            {
+                wifi = true;
+            }
+            else
+            {
+                wifi = false;
+            }
+
+
+            if (model == "roomba")
+            {
+                soap = true;
+            }
+            else if (model == "vinduspudser")
+            {
+                soap = true;
+                Console.WriteLine("Hvor mange hjul vil du ha på");
+                wheels = byte.Parse(Console.ReadLine());
+            }
+            else if (model == "dækskifter")
+            {
+                Console.WriteLine("Hvor mange hjul vil du ha på");
+                wheels = byte.Parse(Console.ReadLine());
+                if (color != "hvid")
+                {
+                    battery = false;
+                }
+                else
+                {
+                    battery = true;
+                    Console.WriteLine("Hvor stort skal batteriet være fra 1 til 255");
+                    batterySize = byte.Parse(Console.ReadLine());
+                }
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+
+            Robots robot = new Robots(model, color, wifi, battery, batterySize, soap, wheels);
+            Console.WriteLine("");
+            Console.WriteLine($"\r\nChip:{robot.Chip}\r\nModel: {robot.Model}\r\nColor: {robot.Color}\r\nWifi: {robot.Wifi}\r\nBattery: {robot.Battery}\r\nBatterySize: {robot.BatterySize}\r\nSoapThingy: {robot.Soap}\r\nWheels: {robot.Wheels}");
+        }
+    }
+}
---
+++ Robotterne kommer/Robots.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robotterne_kommer
+{
+    internal class Robots
+    {
+        //Instance variables
+        private string model;
+        private string color = "Hvid";
+        private string chip;
+        private bool wifi;
+        private byte batterySize = 0;
+        private bool battery;
+        private byte wheels;
+        private bool soap;
+
+
+
+        //Encapsulation
+        public string Model { get { return model; } }
+        public string Color { get { return color; } }
+        public string Chip { get { return chip; } }
+        public bool Wifi { get { return wifi; } }
+        public byte BatterySize { get { return batterySize; } }
+        public bool Battery { get { return battery; } }
+        public byte Wheels { get { return wheels; } }
+        public bool Soap { get { return soap; } }
+
+
+        public Robots(string model)
+        {
+            Random rng = new Random();
+            int choice = rng.Next(0, 2);
+            this.chip = choice == 1 ? "RX54667" : "QT8339";
+            this.model = model;
+        }
+        public Robots(string model, string color) : this(model)
+        {
+            this.color = color;
+        }
+        public Robots(string model, string color, bool wifi ) : this(model, color)
+        {
+            this.wifi = wifi;
+        }
+        public Robots(string model, string color, bool wifi, bool battery, byte batterySize, bool soap, byte wheels) : this(model, color, wifi)
+        {
+            this.battery = battery;
+            this.batterySize = batterySize;
+            this.soap = soap;
+            this.wheels = wheels;
+        }
+
+
+    }
+}
